31e71d07d58988d13e32507c0588e36f9c0157ee
Fernando Arriondo <arriondovfernando@gmail.com>
2022-09-03 15:13:39 -0300

    api: add root route and html

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,14 +1,14 @@
-const router = require('express').Router();
+const router = require("express").Router();
 
 //const { Pokemon } = require('../models/Pokemon.js');
 //const { Tipo } = require('../models/Tipo.js');
 
-router.get('/', function(req,res){
-
-})
+router.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "/index.html"));
+});
 
 module.exports = {
-    pokemons: require('./pokemons.js'),
-    types: require('./types.js'),
-    index: router
-}
+  pokemons: require("./pokemons.js"),
+  types: require("./types.js"),
+  index: router,
+};
