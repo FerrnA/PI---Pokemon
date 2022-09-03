d8e0756df6adada164f6942e8a9737a031ed6994
Fernando Arriondo <arriondovfernando@gmail.com>
2022-09-03 19:51:14 -0300

    api: same

--- api/src/app.js
+++ api/src/app.js
@@ -18,7 +18,7 @@ server.use(express.json({ limit: "50mb" }));
 server.use(cookieParser());
 server.use(morgan("dev"));
 server.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", `${clientUrl}`);
+  res.header("Access-Control-Allow-Origin", "https://pokeapi-fer.vercel.app/");
   res.header("Access-Control-Allow-Credentials", "true");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
