2427741fd2c79750be52f24cf07e1a039773f78c
Fernando Arriondo <arriondovfernando@gmail.com>
2022-09-03 19:48:57 -0300

    api: cors acc-cont-all-or try

--- api/src/app.js
+++ api/src/app.js
@@ -1,39 +1,38 @@
-const express = require('express');
-const cookieParser = require('cookie-parser');
+const express = require("express");
+const cookieParser = require("cookie-parser");
 /* const bodyParser = require('body-parser'); */
-const morgan = require('morgan');
+const morgan = require("morgan");
 
-//1 const routes = require('./routes/index.js');
-const { pokemons, types, index } = require('./routes/index.js');
+const { pokemons, types, index } = require("./routes/index.js");
 
-
-require('./db.js');
+require("./db.js");
 
 const server = express();
 
-const clientUrl = process.env.API_CLIENT_URL
+const clientUrl = "https://pokeapi-fer.vercel.app/";
 
-server.name = 'API';
+server.name = "API";
 
-server.use(express.urlencoded({ extended: true, limit: '50mb' })); //bodyParser.urlencoded()
-server.use(express.json({ limit: '50mb' })); //bodyParser.json()
+server.use(express.urlencoded({ extended: true, limit: "50mb" }));
+server.use(express.json({ limit: "50mb" }));
 server.use(cookieParser());
-server.use(morgan('dev'));
+server.use(morgan("dev"));
 server.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', `${clientUrl}`); // http://localhost:3001?-update to match the domain you will make the request from
-  res.header('Access-Control-Allow-Credentials', 'true');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+  res.header("Access-Control-Allow-Origin", `${clientUrl}`);
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
   next();
 });
 
-//1 server.use('/', router);
-server.use('/', index);
-server.use('/pokemons', pokemons);
-server.use('/types', types);
+// server routes
+server.use("/", index);
+server.use("/pokemons", pokemons);
+server.use("/types", types);
 
 // Error catching endware.
-server.use((err, req, res, next) => { // eslint-disable-line no-unused-vars
+server.use((err, req, res, next) => {
+  // eslint-disable-line no-unused-vars
   const status = err.status || 500;
   const message = err.message || err;
   console.error(err);
