8639ce55383545643c329f5b5543b087a7cdfd4d
Fernando Arriondo <arriondovfernando@gmail.com>
2022-09-03 03:52:34 -0300

    api: listen port env or 80

--- api/index.js
+++ api/index.js
@@ -17,22 +17,24 @@
 //     =====`-.____`.___ \_____/___.-`___.-'=====
 //                       `=---='
 //     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-const server = require('./src/app.js');
-const { conn, Tipo } = require('./src/db.js');
-const axios = require('axios');
+const server = require("./src/app.js");
+const { conn, Tipo } = require("./src/db.js");
+const axios = require("axios");
+
+const port = process.env.PORT || 80;
 
 // Syncing all the models at once.
-conn.sync({ force: true }).then( async () => {
-  server.listen(3001, () => {
-    console.log('%s listening at 3001'); // eslint-disable-line no-console
+conn.sync({ force: true }).then(async () => {
+  server.listen(port, () => {
+    console.log(`%s listening at ${port}`); // eslint-disable-line no-console
   });
   /**/
   let estantipos = await Tipo.findAll();
-  if(estantipos.length === 0){
-    let tipostraidosapi = await axios({url: 'https://pokeapi.co/api/v2/type'})
-        .then(response => response.data)
-        .then(resp => resp.results);
-    let creadores = tipostraidosapi.map(data => Tipo.create({name: data.name}));
+  if (estantipos.length === 0) {
+    let tipostraidosapi = await axios({ url: "https://pokeapi.co/api/v2/type" })
+      .then((response) => response.data)
+      .then((resp) => resp.results);
+    let creadores = tipostraidosapi.map((data) => Tipo.create({ name: data.name }));
     await Promise.all(creadores);
     console.log("Tipos precargados");
   }
