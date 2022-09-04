05d014b4f7c3d06ab206294e5bda590f567d9115
Fernando Arriondo <arriondovfernando@gmail.com>
2022-09-03 21:07:17 -0300

    api: same cors

--- api/src/app.js
+++ api/src/app.js
@@ -1,37 +1,37 @@
-const express = require("express");
-const cookieParser = require("cookie-parser");
+const express = require('express');
+const cookieParser = require('cookie-parser');
 /* const bodyParser = require('body-parser'); */
-const morgan = require("morgan");
+const morgan = require('morgan');
 
-const { pokemons, types, index } = require("./routes/index.js");
+const { pokemons, types, index } = require('./routes/index.js');
 
-require("./db.js");
+require('./db.js');
 
 const server = express();
 
-const clientUrl = "https://pokeapi-fer.vercel.app/";
+const clientUrl = 'https://pokeapi-fer.vercel.app/';
 
-server.name = "API";
+server.name = 'API';
 
-server.use(express.urlencoded({ extended: true, limit: "50mb" }));
-server.use(express.json({ limit: "50mb" }));
+server.use(express.urlencoded({ extended: true, limit: '50mb' }));
+server.use(express.json({ limit: '50mb' }));
 server.use(cookieParser());
-server.use(morgan("dev"));
+server.use(morgan('dev'));
 server.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "https://pokeapi-fer.vercel.app/");
-  res.header("Access-Control-Allow-Credentials", "true");
+  res.header('Access-Control-Allow-Origin', 'https://pokeapi-fer.vercel.app');
+  res.header('Access-Control-Allow-Credentials', 'true');
   res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Origin"
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Origin'
   );
-  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
   next();
 });
 
 // server routes
-server.use("/", index);
-server.use("/pokemons", pokemons);
-server.use("/types", types);
+server.use('/', index);
+server.use('/pokemons', pokemons);
+server.use('/types', types);
 
 // Error catching endware.
 server.use((err, req, res, next) => {
